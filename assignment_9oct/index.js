9fb7676671f343841dc97c5964ecc08143f738cd
Arzuu <aarjudhiman987@gmail.com>
2023-10-12 18:49:15 +0530

    complite

--- assignment_9oct/index.js
+++ assignment_9oct/index.js
@@ -1,19 +1,16 @@
 const navbar = document.querySelector('.header');
 const header = document.querySelector('.rowone');
 const navItems = document.querySelectorAll('.nav-link');
+const toggleButton = document.querySelector('.navbar-toggler-icon')
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
     const headerHeight = navbar.clientHeight;
     const scrollThreshold = headerHeight;
     if (scrollY >= scrollThreshold) {
-        navbar.style.backgroundColor ="#fffff7"
+        navbar.style.backgroundColor ="black"
+        toggleButton.style.backgroundColor="black"
 
-        // navbar.style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.5)';
-    
-        navItems.forEach(navItem => {
-            navItem.style.color = 'black';
-        });
     } else {
         navItems.forEach(navItem => {
             navbar.style.backgroundColor =""
@@ -21,3 +18,4 @@ window.addEventListener('scroll', () => {
         });
     }
 });
+
